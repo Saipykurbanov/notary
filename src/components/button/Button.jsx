a700578c6581ce1be2998c3a9acae42feeb08f36
salimKurbanov <salim.kurbanov08@gmail.com>
2024-10-18 23:17:56 +0300

    spoiler

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './css/button.css';
 
-const Button = ({mode, content}) => {
+const Button = ({mode, content, callback}) => {
     return (
-        <button className={`main_button ${mode}`}>
+        <button className={`main_button ${mode}`} onClick={callback}>
             <div className="content">
                 {content}
             </div>
